402f99e13f802509159a41bc8265ebaa5afe62e0
Ruslan Bairamovi <thebairamov@gmail.com>
2020-04-02 19:20:32 +0400

    menu item component styling

--- src/component/menu-item/menu-item.component.jsx
+++ src/component/menu-item/menu-item.component.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 import './menu-item.component.scss'
 
 const MenuItem = ({title, img, size}) => (
-    <div className={`${size} menu-item`} style={{backgroundImage: `url(${img})`}}>
+    <div className={`${size} menu-item`}>
+        <div className="background-image" style={{backgroundImage: `url(${img})`}}></div>
         <div className="content">
             <h1 className="title">{title}</h1>
             <span className="subtitle">SHOP NOW</span>
