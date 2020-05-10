8b2945a332a9955a6aa48c3db5afedb47162db3c
Ruslan Bairamovi <thebairamov@gmail.com>
2020-05-11 02:29:41 +0400

    update shop data with new data and adding quantity for same items selected multiply times

---
+++ src/redux/cart/cart.utils.js
@@ -0,0 +1,15 @@
+export const addItemToCart = (cartItems, cartItemsToAdd) => {
+    const existing = cartItems.find(
+        cartItem => cartItem.id === cartItemsToAdd.id
+        );
+
+    if(existing) {
+        return cartItems.map(cartItem => 
+            cartItem.id === cartItemsToAdd.id
+            ? {...cartItem, quantity: cartItem.quantity + 1} 
+            : cartItem
+        )
+    }
+
+return [...cartItems, {...cartItemsToAdd, quantity: 1}]
+}
